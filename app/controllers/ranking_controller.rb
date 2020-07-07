72c616d844f7d00c9e77dd3b2324ac301346d875
nelso <nelson.oliveira.sousa@gmail.com>
2020-07-06 22:22:25 -0300

    ranking function

---
+++ app/controllers/ranking_controller.rb
@@ -0,0 +1,39 @@
+class RankingController < ApplicationController
+    before_action :set_global_summary_service
+
+    before_action :set_global_summary_service
+
+    def index
+        @ranking = raking_formated()
+    end
+
+    def set_global_summary_service
+        @sumary_service = GlobalSummary.new
+    end
+
+    def raking_formated()
+       votos =  Like.find_by_sql('
+            SELECT reference_movie_ep_id, 
+            COUNT(reference_movie_ep_id),
+            RANK() OVER(
+                ORDER BY COUNT(reference_movie_ep_id)
+            )
+            FROM likes 
+            group by reference_movie_ep_id
+        ').as_json
+
+        @summary = @sumary_service.general
+
+        ranking = []
+        votos.each do |voto|
+            @summary['results'].each do | suma| 
+                if suma['episode_id'] == voto['reference_movie_ep_id']
+                    ranking << suma.merge(voto)
+                end
+            end
+        end
+
+        ranking
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     
   	
   resources :likes, only: %i[create]
-
+  resources :ranking, only: %i[index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
