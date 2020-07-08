57181dabe28635cf536c5876ac6fb6287cefe92a
nelso <nelson.oliveira.sousa@gmail.com>
2020-07-07 23:13:00 -0300

    Alteração de tradução

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :firstname, length: { in: 3..15 }, presence: true
+  validates :lastname, length: { in: 3..15 }, presence: true
 end
