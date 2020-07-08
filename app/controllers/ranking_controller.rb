1280538c5448493e47c16396bf2c715684f2bad3
nelso <nelson.oliveira.sousa@gmail.com>
2020-07-08 01:36:50 -0300

    Teste Ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -53,7 +53,7 @@ class RankingController < ApplicationController
             votos.each do |voto|
                 puts voto
                if voto['reference_movie_ep_id'].to_s == id
-                    puts voto
+                    puts voto['rank'] <= 2
                     if voto['rank'] <= 2
                         result = true
                     else
